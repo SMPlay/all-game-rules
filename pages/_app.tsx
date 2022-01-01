f5f0688a1653cab8f94051c8d19df145bdcb6182
Pavel Sokolov <49731235+SMPlay@users.noreply.github.com>
2022-01-01 21:45:52 +0300

    Added styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { createGlobalStyle } from "styled-components";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
 
-export default MyApp
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </>
+);
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
+import styled from "styled-components";
 import type { NextPage } from "next";
 
-const Home: NextPage = () => <h1>All Game rules</h1>;
+const Title = styled.h1`
+  color: red;
+`;
+
+const Home: NextPage = () => <Title>All Game rules</Title>;
 
 export default Home;
