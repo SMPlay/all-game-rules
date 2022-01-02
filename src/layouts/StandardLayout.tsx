27ffc1e585bba0d5f05812801dcaaa4d875f5f3e
Pavel Sokolov <49731235+SMPlay@users.noreply.github.com>
2022-01-02 22:08:23 +0300

    Added container to layout

--- src/layouts/StandardLayout.tsx
+++ src/layouts/StandardLayout.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
+import { Container } from "@mui/material";
 
 import { Header } from "components/Header";
 
 export const StandardLayout: FC = ({ children }) => (
   <>
     <Header />
-    {children}
+    <Container>{children}</Container>
   </>
 );
